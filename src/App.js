c2c8d158a72063b2d0729e61ce90f2b1f580c777
democrito88 <d_who@hotmail.com>
2024-02-15 15:11:06 -0300

    15-02-2024

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { Html5QrcodeScanner } from 'html5-qrcode';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [scanResult, setScanResult] = useState(null);
+
+  useEffect(()=>{
+    const scanner = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    });
+    
+    const sucesso = (result) => {
+      scanner.clear();
+      setScanResult(result);
+    }
+    
+    const erro = (error) => {
+      console.warn(error);
+    }
+    
+    scanner.render(sucesso, erro);
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Leitor de código em React</h1>
+      { scanResult ?
+      <div>Mensagem escaneada: 
+        <a href={"http://"+scanResult}>{scanResult}</a>
+      </div>
+      :
+      <div id="reader"></div>
+    }
+
     </div>
   );
 }
